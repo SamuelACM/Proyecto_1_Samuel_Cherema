042cb79122e58de828fc1a730f1fe74772de1f28
SamuelACM <cheremasm@gmail.com>
2024-10-19 11:29:45 -0400

    mostrar numeros pares e inpares con js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
 console.log("Hola mundo");
+
+for (let cont=0; cont<=20.; cont++){
+    if (cont%2 === 0){
+        console.log(cont + " es par");
+    }else {
+        console.log(cont + " es inpar");
+    }
+}

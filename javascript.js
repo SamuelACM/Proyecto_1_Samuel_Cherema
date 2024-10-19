44d252f6f998255b189f5edecc68a742f7e57efc
SamuelACM <cheremasm@gmail.com>
2024-10-19 12:13:58 -0400

    los numeros aparecen en la consola gracias al boton

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 console.log("Hola mundo");
 
+function funcionclic(){
 for (let cont=0; cont<=20.; cont++){
     if (cont%2 === 0){
         console.log(cont + " es par");
     }else {
         console.log(cont + " es inpar");
     }
+ 
+} 
 }
